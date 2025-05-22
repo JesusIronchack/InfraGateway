e83f1cf93123e4627e518349809c586f879d8495
JesusCiber <fjesuscl@gmail.com>
2025-05-22 15:42:22 +0200

    Init commit

---
+++ src/main/java/com/example/InfraGateway/Config/ApiGatewayConfig.java
@@ -0,0 +1,15 @@
+package com.example.InfraGateway.Config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+    }
+}
---
+++ src/test/java/com/example/InfraGateway/InfraGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InfraGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
