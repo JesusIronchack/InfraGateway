9ceedd46271d83bd6a5ff6310b590fd82644567f
JesusCiber <fjesuscl@gmail.com>
2025-05-26 17:48:38 +0200

    corregir errores

--- src/main/java/com/example/InfraGateway/Config/ApiGatewayConfig.java
+++ src/main/java/com/example/InfraGateway/Config/ApiGatewayConfig.java
@@ -5,11 +5,19 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration
 public class ApiGatewayConfig {
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("bike_service", r -> r.path("/api/bikes/**")
+                        .uri("lb://MicroServiceBike"))
+                .route("station_service", r -> r.path("/api/stations/**")
+                        .uri("lb://MicroServiceStation"))
+                .build();
 
     }
 }
